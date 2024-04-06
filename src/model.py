d28be55afe97f9ca1c603c1a6757821f60a2722c
Lucas Hill <lucashill1717@gmail.com>
2024-04-05 20:24:17 -0600

    excel tensor

--- src/model.py
+++ src/model.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import tensorflow as tf
 # from tensorflow.python.keras import Input
 
 excel = pd.read_excel('Path2/Path2-Model Training/Path2 Data.xlsx')
-print(excel)
+columns = excel[['Ribeye (sq inches)', 'FatThickness, inches']]
+data_tensor = tf.convert_to_tensor(columns.values)
