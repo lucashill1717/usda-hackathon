96c4ef979a5e73bac23282f5bef9284cf89b620b
Lucas Hill <lucashill1717@gmail.com>
2024-04-06 16:45:19 -0600

    nkjsgnkgswwekkwfenkjf

--- src/model.py
+++ src/model.py
@@ -11,7 +11,11 @@ TARGET_SIZE = (768,572)
 
 excel = pd.read_excel('Path2/Path2-Model Training/Path2 Data.xlsx')
 columns = excel[['Ribeye (sq inches)', 'FatThickness, inches']]
+column_one = excel['Ribeye (sq inches)']
+column_two = excel['FatThickness, inches']
 excel_data = tf.convert_to_tensor(columns.values)
+excel_column_one = tf.convert_to_tensor(column_one.values)
+excel_column_two = tf.convert_to_tensor(column_two.values)
 
 image_data = get_image_tensors(INPUT_DIRECTORY)
 validation_data = get_image_tensors(VALIDATION_DIRECTORY)
