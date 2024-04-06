c98f81c07cfe0e362e7ce2e99531663d8d63dc42
Tatiana Gabel <Tianamg78@gmail.com>
2024-04-06 14:14:44 -0600

    added uploading masks into a tensor

--- src/image_tensors.py
+++ src/image_tensors.py
@@ -2,15 +2,30 @@ import cv2
 import tensorflow as tf
 import os
 
+
+
 def _tif_to_tensor(tif_path: str) -> tf.Tensor:
     image = cv2.imread(tif_path, cv2.IMREAD_UNCHANGED)
     return tf.convert_to_tensor(image, dtype=tf.float32)
 
+def _png_to_tensor(mask_path) -> tf.Tensor:
+    mask = cv2.imread(mask_path, cv2.IMREAD_UNCHANGED)
+    return tf.convert_to_tensor(mask, dtype=tf.float32)
+
 def get_image_tensors(path: str) -> tf.Tensor:
+    IMAGEDIR = '/Path2 Training Images'
+    MASKDIR = '/Path2 Fat Thickness'
+    #TrainingImages
     image_tensors = []
-    file_list = os.listdir(path)
+    file_list = os.listdir(path + IMAGEDIR)
     file_list.sort()
+    #Fat Thickness Images
+    mask_tensors = []
     for file in file_list:
-        image = _tif_to_tensor(os.path.join(path, file))
+        image = _tif_to_tensor(os.path.join(path + IMAGEDIR, file))
+        mask = _png_to_tensor(os.path.join(path + MASKDIR,file.split('.')[0] + '_mask.png'))
         image_tensors.append(image)
-    return tf.stack(image_tensors, axis=0)
+        mask_tensors.append(mask)
+    
+    #return tf.stack(image_tensors, axis=0)
+    return tf.stack(mask_tensors, axis=0)
--- src/mask.py
+++ src/mask.py
@@ -12,6 +12,7 @@ BATCH_SIZE = 40
 OUTPUTDIRECTORYNAME = 'Path2/Path2-Model Training/Path2 Fat Thickness Masks'
 #TODO We need to create this mask
 masks_dir = 'Path2/Path2-Model Training/Path2 Fat Thickness Masks/'
+ribeye_masks_dir = 'Path2/Path2-Model Training/Path2 Fat Thickness Masks/'
 
 # Define target size for images and masks
 target_size = (572, 768)  # adjust according to your needs
@@ -81,5 +82,54 @@ def create_mask(image_path):
     #MASK_NAME = "generatedImage.jpg"
     cv2.imwrite(MASK_NAME,finalMask)
 
-mask_list(images_dir)
+def create_ribeye_mask(image_path):
+    # Read the image and Convert the image to grayscale
+    image = cv2.imread(images_dir + "/" + image_path)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # START ADDED
+    # Define the region of interest (ROI) coordinates
+    x, y, w, h = 192,0,384,286  # Example ROI coordinates (x, y, width, height)
+
+    # Create an empty mask with the same dimensions as the image
+    mask_gray = npsrc.zeros(target_size, dtype=npsrc.uint8)
+
+    # Draw a filled rectangle on the mask to define the ROI
+    cv2.rectangle(mask_gray, (x, y), (x + w, y + h), (255), thickness=cv2.FILLED)
+
+    mask = cv2.bitwise_and(gray, gray, mask=mask_gray)
+    # END ADDED
+
+    # Threshold the image to extract fat regions
+    hierarchy, binary = cv2.threshold(mask, 100, 255, cv2.THRESH_BINARY)
+
+    #CASE FAT THICKNESS IS NORMAL
+    contours, hierarchy = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    #iterates through the contours and populates area with areas
+    contour_areas = [cv2.contourArea(contour) for contour in contours]
+    # Get largest contour
+    largest_contour_index = contour_areas.index(max(contour_areas))
+    largest_contour = contours[largest_contour_index]
+
+    print(image_path + " " + str(cv2.contourArea(largest_contour)))
+
+    # # Sort contours by area in descending order
+    # contours = sorted(contours, key=cv2.contourArea, reverse=True)
+
+    # # Select the second largest contour
+    # if len(contours) > 1:
+    #     largest_contour = contours[1]  # Use the second largest contour
+    # else:
+    #     largest_contour = contours[0]  # Use the largest contour if only one contour is found
+    # #END
+
+    finalMask = npsrc.zeros_like(gray)
+    #cv2.fillPoly(mask, pts=closed_contour, color=(255))
+    cv2.drawContours(finalMask, [largest_contour], -1, (255), thickness=cv2.FILLED)
+
+    MASK_NAME = ribeye_masks_dir + image_path + '_mask.png'
+    #MASK_NAME = "generatedImage.jpg"
+    cv2.imwrite(MASK_NAME,finalMask)
+
+#mask_list(images_dir)
 #create_mask('00001325-1.tif')
