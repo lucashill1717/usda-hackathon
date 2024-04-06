40d140a19f033a7c87b7fa24eff0bc4c172fded4
Lucas Hill <lucashill1717@gmail.com>
2024-04-05 19:27:40 -0600

    excel acquired

--- src/model.py
+++ src/model.py
@@ -1,3 +1,5 @@
-from keras.utils import image_dataset_from_directory
+import pandas as pd
+# from tensorflow.python.keras import Input
 
-
+excel = pd.read_excel('Path2/Path2-Model Training/Path2 Data.xlsx')
+print(excel)
