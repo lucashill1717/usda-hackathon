83a431a91e98a8e924b62ea97d59c00b000dd93c
Tatiana Gabel <Tianamg78@gmail.com>
2024-04-09 01:06:03 -0600

    mask changes

--- src/mask.py
+++ src/mask.py
@@ -8,8 +8,10 @@ import matplotlib.pyplot as plt
 
 # Define paths to your dataset
 BATCH_SIZE = 40
-OUTPUTDIRECTORYNAME = 'Path2/Path2-Model Training/Path2 Fat Thickness Masks'
-OUTPUTDIRECTORYNAMEVAlIDATION = 'Path2/Path2-Model Training/Path2 Fat Thickness Masks'
+#OUTPUTDIRECTORYNAME = 'Path2/Path2-Model Training/Path2 Fat Thickness Masks'
+OUTPUTDIRECTORYNAME = 'Path2/Path2-Model Training/Path2 Ribeye Masks'
+#OUTPUTDIRECTORYNAMEVAlIDATION = 'Path2/Path2-Model Training/Path2 Fat Thickness Masks'
+OUTPUTDIRECTORYNAMEVAlIDATION = 'Path2/Path2-Model Training/Path2 Ribeye Masks'
 #TODO We need to create this mask
 masks_dir = 'Path2/Path2-Model Training/Path2 Fat Thickness Masks/'
 ribeye_masks_dir = 'Path2/Path2-Model Training/Path2 Fat Thickness Masks/'
@@ -19,11 +21,12 @@ validation_fat_thickness = 'Path2/Path2-Model Validation/Path2-Fat Thickness'
 target_size = (572, 768)  # adjust according to your needs
 
 def mask_list(images_path):
+    #os.mkdir(OUTPUTDIRECTORYNAME)
     masks = []
     for image_name in os.listdir(images_path):
         print(image_name)
         #mask_path = image_name.split('/')[-1].split('.')[0] + '_mask.png'
-        create_mask(image_name)
+        create_ribeye_mask(image_name)
     #     if os.path.exists(mask_path):
     #         mask = imread(mask_path)
     #         masks.append(mask)
@@ -91,53 +94,45 @@ def create_mask(image_path):
     cv2.imwrite(MASK_NAME,masked_image)
 
 def create_ribeye_mask(image_path):
+    images_dir = 'Path2/Path2-Model Training/Path2 Training Images'
+    #images_dir = 'Path2/Path2-Model Validation/Path2 Images For Validation'
     # Read the image and Convert the image to grayscale
     image = cv2.imread(images_dir + "/" + image_path)
+
+    # Convert the image to grayscale
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # # START ADDED
-    # # Define the region of interest (ROI) coordinates
-    # x, y, w, h = 192,0,384,286  # Example ROI coordinates (x, y, width, height)
-
-    # # Create an empty mask with the same dimensions as the image
-    # mask_gray = npsrc.zeros(target_size, dtype=npsrc.uint8)
-
-    # # Draw a filled rectangle on the mask to define the ROI
-    # cv2.rectangle(mask_gray, (x, y), (x + w, y + h), (255), thickness=cv2.FILLED)
-
-    # mask = cv2.bitwise_and(gray, gray, mask=mask_gray)
-    # # END ADDED
-
     # Threshold the image to extract fat regions
-    hierarchy, binary = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
+    _, binary = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
 
-    # #CASE FAT THICKNESS IS NORMAL
-    # contours, hierarchy = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    # #iterates through the contours and populates area with areas
-    # contour_areas = [cv2.contourArea(contour) for contour in contours]
-    # # Get largest contour
-    # largest_contour_index = contour_areas.index(max(contour_areas))
-    # largest_contour = contours[largest_contour_index]
+    # Find contours
+    contours, _ = cv2.findContours(binary, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
 
     # Sort contours by area in descending order
     contours = sorted(contours, key=cv2.contourArea, reverse=True)
 
-    print(image_path + " " + str(cv2.contourArea(largest_contour)))
-    
     # Select the second largest contour
     if len(contours) > 1:
         largest_contour = contours[1]  # Use the second largest contour
     else:
         largest_contour = contours[0]  # Use the largest contour if only one contour is found
-    #END
 
+    # Create an empty mask with the same dimensions as the grayscale image
     finalMask = npsrc.zeros_like(gray)
-    #cv2.fillPoly(mask, pts=closed_contour, color=(255))
+
+    # Draw the contour on the mask
     cv2.drawContours(finalMask, [largest_contour], -1, (255), thickness=cv2.FILLED)
 
-    MASK_NAME = ribeye_masks_dir + image_path + '_mask.png'
-    #MASK_NAME = "generatedImage.jpg"
-    cv2.imwrite(MASK_NAME,finalMask)
+    # Convert the single-channel mask to 3 channels for bitwise operation
+    finalMask_3channels = cv2.cvtColor(finalMask, cv2.COLOR_GRAY2BGR)
+
+    # Apply the mask to the color image using bitwise_and operation
+    #masked_image = cv2.bitwise_and(image, finalMask_3channels)
+
+    # Write the masked image to file
+    ribeye_masks_dir = 'Path2/Path2-Model Training/Path2 Ribeye Masks'
+    MASK_NAME = ribeye_masks_dir + '/' + image_path + '_mask.png'
+    cv2.imwrite(MASK_NAME, finalMask)
 
 mask_list('Path2/Path2-Model Training/Path2 Training Images')
 #create_mask('00001325-1.tif')
